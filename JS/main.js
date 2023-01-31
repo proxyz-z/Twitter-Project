ac81ee493673e1b5731ad6332f91cb29d254236e
Yousef Essam <124045790+proxyz-z@users.noreply.github.com>
2023-01-31 18:18:49 +0200

    js checks
    .

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,8 @@
 let btn = document.getElementsByClassName('mainBtn')[0];
 let main = document.getElementsByClassName('main')[0];
+let mainInp = document.getElementById('mainInput');
 btn.addEventListener('click', function() {
-    main.style.height = '20vh';
+    if(!mainInp.value == '') {
+        main.style.height = '10vh';
+    }
 });
