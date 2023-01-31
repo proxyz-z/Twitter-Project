68f70bb791fd58def564649db104da5a5850ced2
Yousef Essam <124045790+proxyz-z@users.noreply.github.com>
2023-01-31 17:57:46 +0200

    Basic design
    added image , input and button basic design

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+let btn = document.getElementsByClassName('mainBtn')[0];
+let main = document.getElementsByClassName('main')[0];
+btn.addEventListener('click', function() {
+    main.style.height = '20vh';
+});
